2265daa32e27aa91aad614eddfe6e49552398f83
Brett Swanson <bswanson@cochlear.com>
2024-11-20 15:10:30 +1100

    SRES-272: Refactor: Get_coefficients_proc does not have much use outside of this demo, so merge into FFT_VS_FIR_equivalence_demo. FIR_filterbank_proc assumes some other function sets the filter coefficients.

--- matlab/Filterbank/FFT_VS_FIR_equivalence_demo.m
+++ matlab/Filterbank/FFT_VS_FIR_equivalence_demo.m
@@ -2,31 +2,47 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Copyright: Cochlear Ltd
-%   Authors: Brett Swanson
+%   Authors: Brett Swanson, Jamon Windeyer
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 p = struct;
 p.audio_sample_rate_Hz = 16000;
 p.analysis_rate_Hz = 16000;
 
-p_aud = Audio_proc(p);
-p_fftv = FFT_VS_filterbank_proc(p);
-p_fir = Get_coefficients_proc(p_fftv);
+pa = Audio_proc(p);
+pv = FFT_VS_filterbank_proc(p);
+
+% Calculate complex FIR coefficients (impulse response) for each band:
+bin_coeffs = zeros(pv.block_length/2 + 1, pv.block_length);
+nn = (0:(pv.block_length - 1))';
+rr = -2i * pi * nn / pv.block_length; % factor out common sub-expression from loopv.
+K = pv.block_length/2;
+for k = 0:K
+    % Each bin impulse response is a modulated window function:
+    bin_coeffs(k + 1, :) = pv.window .* exp(rr * k); 
+end
+% Band impulse responses are weighted sums of bin impulse responses:
+band_coeffs = pv.weights * bin_coeffs;
+% When considering as traditional FIR, the coefficients are reversed:
+band_coeffs = flip(band_coeffs, 2);
+for n = 1:pv.num_bands
+    pf.numer{n} = band_coeffs(n, :);
+end
 
 %% Process audio:
-audio = Audio_proc(p_aud, 'asa.wav');
-u_fftv = FFT_VS_filterbank_proc(p_fftv, audio);
-% FIR output:
-u_fir = zeros(p_fir.num_bands, length(audio));
-for band = 1:p_fir.num_bands
-    u_fir(band, :) = filter(p_fir.band_coeffs(band, :), 1, audio);
-end
+audio = Audio_proc(pa, 'asa.wav');
+uv = FFT_VS_filterbank_proc(pv, audio);
+uf = FIR_filterbank_proc(pf, audio);
+
+%% Quantify differences:
+u_delta = uv - uf; % complex
+rms_delta = rms(u_delta, "all")
 
 %% Plot shows no difference:
 figure, hold on;
-plot(real(u_fftv(1, :)), 'r');
-plot(real(u_fir (1, :)), 'b');
+plot(real(uv(1, :)), 'r');
+plot(real(uf(1, :)), 'b');
 zoom xon
 
 %%
-GUI_FTM(p_fftv, {u_fftv, u_fir}, {'FFT VS', 'CFIR'});
+GUI_FTM(pv, {uv, uf}, {'FFT VS', 'CFIR'});
