7072fc417829d51ec76db9e65c0bd61c18a0ac31
Brett Swanson <bswanson@cochlear.com>
2025-10-23 20:51:29 +1100

    SRES-272: Refactor: separate out Audio_calibration_proc from Audio_proc.

---
+++ matlab/FrontEnd/Audio_calibration_proc.m
@@ -0,0 +1,78 @@
+function [x, calibration_gain_dB, audio_dB_SPL] = Audio_calibration_proc(p, a)
+
+% Audio_proc: Calculate and apply calibration gain.
+%
+% [x, calibration_gain_dB, audio_dB_SPL] = Audio_proc(p, a)
+%
+% There are two alternative methods for calibrating sound levels:
+% 
+% 1. Set p.audio_dB_SPL to the desired sound pressure level (default 65 dB SPL).
+%    In this case, calibration_gain_dB will be calculated from the input audio 
+%    to give the desired sound pressure level (relative to reference_dB_SPL).
+%    The important return value is the calculated calibration_gain_dB 
+%    (the returned audio_dB_SPL will be equal to p.audio_dB_SPL).
+%
+% 2. Set p.calibration_gain_dB to the desired gain in dB.
+%    In this case, this pre-determined calibration gain will be applied to the input audio.
+%    The important return value is the calculated audio_dB_SPL 
+%    (the returned calibration_gain_dB will be equal to p.calibration_gain_dB).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    Copyright: Cochlear Ltd
+%      Authors: Brett Swanson
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+switch nargin
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+case 0	% Default parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	x = feval(mfilename, struct);
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+case 1	% Parameter calculations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % A reference pure tone with amplitude +/-1.0 is defined to represent a certain Sound Pressure Level:
+    p = Ensure_field(p, 'reference_dB_SPL',             95);
+    
+	check(p);
+
+    if ~isfield(p, 'calibration_gain_dB')
+        % The desired audio Sound Pressure Level (the audio data will be scaled to produce this level):
+	    p = Ensure_field(p, 'audio_dB_SPL',             65);
+    end
+
+	x = p;
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+case 2	% Processing
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+	check(p);
+
+	% Calculate the sound pressure level represented by the wav file before scaling:
+    audio_dB_SPL = Calibrate_dB_SPL(p, a);
+
+    if isfield(p, 'calibration_gain_dB')
+        calibration_gain_dB = p.calibration_gain_dB;
+    else
+        % Calculate the calibration gain that will produce the desired sound pressure level:
+        calibration_gain_dB = p.audio_dB_SPL - audio_dB_SPL;
+    end
+
+    % Apply gain:
+    calibration_gain = From_dB(calibration_gain_dB);
+    audio_dB_SPL = audio_dB_SPL + calibration_gain_dB;
+    x = a * calibration_gain;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+end
+
+function check(p)
+	if isfield(p, 'calibration_gain_dB') && isfield(p, 'audio_dB_SPL')
+		error("Can only specify one of calibration_gain_dB or audio_dB_SPL");
+	end
+end
---
+++ matlab/FrontEnd/Test_audio_calibration.m
@@ -0,0 +1,88 @@
+classdef Test_audio_calibration < matlab.unittest.TestCase
+
+% Test_audio_calibration: Class-based test of Calibrate_dB_SPL, Audio_calibration_proc.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright: Cochlear Ltd
+%   Authors: Brett Swanson
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	properties
+        tone_ref;
+		tol = 1e-6; 
+    end
+
+    methods (TestClassSetup)
+		function set_up_tone(testCase)
+			% Generate a full scale reference tone:
+			testCase.tone_ref = Gen_tone(250, 0.1, 16000);
+			testCase.verifyEqual(max(abs(testCase.tone_ref)), 1.0);
+        end
+    end
+
+	methods (Test)
+
+		function tone_audio_dB_SPL(testCase)
+			% Specify p.audio_dB_SPL.
+			
+			p = struct;
+			p = Audio_calibration_proc(p);
+            testCase.verifyEqual(p.reference_dB_SPL, 95);
+            testCase.verifyEqual(p.audio_dB_SPL, 65);
+
+			% By definition, this is reference_dB_SPL: 
+			tone_ref_dB = Calibrate_dB_SPL(p, testCase.tone_ref);
+			testCase.verifyEqual(tone_ref_dB, p.reference_dB_SPL, AbsTol=testCase.tol);
+
+			% Calibrate the tone to the specified level:
+			[tone_65, gain_dB, audio_dB_SPL] = Audio_calibration_proc(p, testCase.tone_ref);
+			% Output tone should be 30 dB down from full scale:
+			testCase.verifyEqual(gain_dB, -30, AbsTol=testCase.tol);
+			testCase.verifyEqual(audio_dB_SPL, 65, AbsTol=testCase.tol);
+			testCase.verifyEqual(testCase.tone_ref * From_dB(gain_dB), tone_65, AbsTol=testCase.tol);
+			tone_ref_dB = Calibrate_dB_SPL(p, tone_65);
+			testCase.verifyEqual(tone_ref_dB, p.audio_dB_SPL, AbsTol=testCase.tol);
+
+			% If the amplitude of the input tone is changed, the gain compensates,
+			% so that the output is still at the desired level:
+			[tone_65a, gain2_dB, audio_dB_SPL] = Audio_calibration_proc(p, testCase.tone_ref / 2);
+			testCase.verifyEqual(tone_65a, tone_65, AbsTol=testCase.tol);
+			testCase.verifyEqual(gain2_dB, gain_dB + To_dB(2), AbsTol=testCase.tol);
+			testCase.verifyEqual(audio_dB_SPL, 65);
+
+			% Change desired output level:
+			p = struct;
+			p.audio_dB_SPL = 85;
+			p = Audio_calibration_proc(p);
+			[tone_85, gain_dB, audio_dB_SPL] = Audio_calibration_proc(p, tone_65);
+			testCase.verifyEqual(gain_dB, 20, testCase.tol);
+			testCase.verifyEqual(audio_dB_SPL, 85, testCase.tol);
+			tone_85_dB = Calibrate_dB_SPL(p, tone_85);
+			testCase.verifyEqual(tone_85_dB, 85, AbsTol=testCase.tol);
+			testCase.verifyEqual(tone_85, 10 * tone_65, AbsTol=testCase.tol);
+			testCase.verifyEqual(max(abs(tone_85)), From_dB(-10), AbsTol=testCase.tol);
+		end
+
+		function tone_calibration_gain_dB(testCase)
+			% Specify p.calibration_gain_dB.
+
+			p = struct;
+			p.calibration_gain_dB = -20;
+			p = Audio_calibration_proc(p);
+			[tone_out, gain_dB, audio_dB_SPL] = Audio_proc(p, testCase.tone_ref);
+			testCase.verifyEqual(gain_dB, -20, AbsTol=testCase.tol);
+			testCase.verifyEqual(audio_dB_SPL, 75, AbsTol=testCase.tol);
+			testCase.verifyEqual(tone_out, testCase.tone_ref * From_dB(gain_dB), AbsTol=testCase.tol);
+			tone_ref_dB = Calibrate_dB_SPL(p, tone_out);
+			testCase.verifyEqual(tone_ref_dB, 75, AbsTol=testCase.tol);
+
+			% If the amplitude of the input tone is reduced, the output is reduced:
+			[tone_out2, gain2_dB, audio2_dB_SPL] = Audio_proc(p, testCase.tone_ref/2);
+			testCase.verifyEqual(tone_out2, tone_out/2, AbsTol=testCase.tol);
+			testCase.verifyEqual(gain2_dB, -20, AbsTol=testCase.tol);
+			testCase.verifyEqual(audio2_dB_SPL, 75 - To_dB(2), AbsTol=testCase.tol);
+			tone_ref_dB = Calibrate_dB_SPL(p, tone_out2);
+			testCase.verifyEqual(tone_ref_dB, 75 - 6, AbsTol=0.1);
+		end
+	end
+end
